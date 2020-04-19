496dbc27e271363c245ff11f3d1ed6561a47cbad
Michael Dorst <m@mdorst.net>
2020-04-19 01:01:36 -0700

    Get json from server
    Add reqwest and serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-  println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+  message: String,
+}
+
+fn main() -> reqwest::Result<()> {
+  let url = "http://localhost:9000";
+
+  println!("GET {}", url);
+
+  let message: Message = reqwest::blocking::get(url)?.json()?;
+
+  println!("{:?}", message);
+  Ok(())
 }
